77ea0e387b76589b1c0bfd8793bab79d050c472d
Jitterbug <3130448+JuniorIsAJitterbug@users.noreply.github.com>
2025-01-31 16:13:26 +0000

    Remove old/broken packages

--- default.nix
+++ default.nix
@@ -16,11 +16,9 @@
   domesdayduplicator = pkgs.callPackage ./pkgs/domesdayduplicator { };
   ltfs = pkgs.callPackage ./pkgs/ltfs { };
   misrc-extract = pkgs.callPackage ./pkgs/misrc-extract { };
-  pyhht = pkgs.python3.pkgs.callPackage ./pkgs/pyhht { };
   qwt = pkgs.callPackage ./pkgs/qwt { useQt6 = false; };
   stfs = pkgs.callPackage ./pkgs/stfs { };
   tbc-video-export = pkgs.python3.pkgs.callPackage ./pkgs/tbc-video-export { };
-  #tzpfms = pkgs.callPackage ./pkgs/tzpfms { };
   vapoursynth-bwdif = pkgs.callPackage ./pkgs/vapoursynth-bwdif { };
   vapoursynth-neofft3d = pkgs.callPackage ./pkgs/vapoursynth-neofft3d { };
   vapoursynth-vsrawsource = pkgs.callPackage ./pkgs/vapoursynth-vsrawsource { };
