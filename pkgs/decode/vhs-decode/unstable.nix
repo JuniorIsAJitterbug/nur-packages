127222be0a9db3ebe9b88d736096aa9af4d7f824
Jitterbug <3130448+JuniorIsAJitterbug@users.noreply.github.com>
2026-01-26 13:36:17 +0000

    vhs-decode-unstable: 0.3.8.1-unstable-2026-01-20 -> 0.3.8.1-unstable-2026-01-24
    Diff: https://github.com/oyvindln/vhs-decode/compare/091c4680e9b60e6056022949e3714ac3eec1f183...76a11bc47e973810ff3d995d59a92ef42eda2595

--- pkgs/decode/vhs-decode/unstable.nix
+++ pkgs/decode/vhs-decode/unstable.nix
@@ -6,10 +6,10 @@
 }:
 let
   pname = "vhs-decode-unstable";
-  version = "0.3.8.1-unstable-2026-01-20";
+  version = "0.3.8.1-unstable-2026-01-24";
 
-  rev = "091c4680e9b60e6056022949e3714ac3eec1f183";
-  hash = "sha256-ArlUfJMD5skWdTDKO1qFTE5bNTeQMPIeeVfhnr05MQQ=";
+  rev = "76a11bc47e973810ff3d995d59a92ef42eda2595";
+  hash = "sha256-/fP0E6c5EzqUlSYfvZy/zHP30lORa9rbCL3NaYfvPGY=";
   cargoHash = "sha256-fKAqjvx4Gqa426OyR2qEPXUPEneXGOT1GqOMFDol0Zc=";
 in
 (vhs-decode.overrideAttrs (
